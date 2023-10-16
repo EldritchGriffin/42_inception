41578d6c05167c42f466d1198800896ba0c3c058
Abdelhak Elyakoubi <aelyakou@e3r11p8.1337.ma>
2023-10-16 01:07:48 +0100

    trying to fix mariadb

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mysql/mariadb.conf.d/50-server.cnf
-
 service mariadb start
 
 echo    "CREATE DATABASE IF NOT EXISTS $DB_NAME;" | mysql -u root
@@ -14,9 +12,7 @@ echo    "FLUSH PRIVILEGES;" | mysql -u root
 
 echo    "ALTER USER 'root'@'localhost' IDENTIFIED BY '$DB_ROOTPWD';" | mysql -u root
 
-kill    $(cat /var/run/mysqld/mysqld.pid)
-
-mysqld_safe
+mysqld
 
 
 
