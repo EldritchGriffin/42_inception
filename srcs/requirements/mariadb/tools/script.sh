1f77b057e174127f274b07d512438831e327262e
Abdelhak Elyakoubi <aelyakou@e3r7p15.1337.ma>
2023-10-17 10:11:38 +0100

    finished mandatory, started working on static website

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -8,7 +8,7 @@ service mariadb start
 SQL_FILE="/tmp/wordpress.sql"
 
 # Create SQL queries in the file
-cat <<EOF > "$SQL_FILE"
+cat << EOF > "$SQL_FILE"
 CREATE DATABASE IF NOT EXISTS $DB_NAME;
 CREATE USER IF NOT EXISTS $DB_USER@'%' IDENTIFIED BY '$DB_PASSWORD';
 GRANT ALL PRIVILEGES ON $DB_NAME.* TO $DB_USER@'%';
@@ -18,6 +18,8 @@ EOF
 # Execute the SQL commands using the mysql client
 mysql < "$SQL_FILE"
 rm "$SQL_FILE"
+
+sleep 5
 
 service mariadb stop
 
